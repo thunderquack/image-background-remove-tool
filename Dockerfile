f3bb87b699a93058359d4087323822a11fbe7d71
thunderquack <egor.i.ivanov@gmail.com>
2021-03-26 21:38:35 +0100

    changes in docker

--- Dockerfile
+++ Dockerfile
@@ -5,37 +5,17 @@ LABEL Author=ty@pidor.de
 RUN apt-get update -y && \
     apt-get install
 
-RUN pip3 install pywebview==3.2 --no-cache-dir
+RUN apt-get install -y libwxgtk3.0-dev libwxgtk3.0-gtk3-dev libuchardet-dev \
+                        libarchive-dev libxerces-c-dev libspdlog-dev libssh-dev \
+                        libsmbclient-dev libnfs-dev
 
-RUN pip3 install numpy==1.18.2 --no-cache-dir
+RUN wget https://github.com/unxed/far2l-deb/raw/master/far2l_2.2%7Eubuntu18.04_amd64.deb
 
-RUN pip3 install tqdm==4.43.0 --no-cache-dir
-
-RUN pip3 install opencv_contrib_python==4.5.1.48 --no-cache-dir
-
-RUN pip3 install scipy==1.6.2 --no-cache-dir
-
-RUN pip3 install torch==1.8.0 --no-cache-dir
-
-RUN pip3 install torchvision==0.9.0 --no-cache-dir
-
-RUN pip3 install gluoncv==0.7.0 --no-cache-dir
-
-RUN pip3 install gdown==3.11.1 --no-cache-dir
-
-RUN pip3 install scikit_image==0.18.1 --no-cache-dir
-
-RUN pip3 install Pillow==7.2.0 --no-cache-dir
-
-RUN pip3 install mxnet==1.6.0 --no-cache-dir
-
-RUN pip3 install flask==1.1.2 --no-cache-dir
-
-RUN pip3 install tensorflow==2.4.1 --no-cache-dir
-
-#RUN pip3 install -r /app/requirements.txt --no-cache-dir
+RUN dpkg -i far2l_2.2~ubuntu18.04_amd64.deb >> /dev/null
 
 COPY ./app /app
+
+RUN pip3 install -r /app/requirements.txt --no-cache-dir
 
 WORKDIR /app
 
--- web/web.py
+++ web/web.py
@@ -1,7 +1,19 @@
 # web/web.py
 
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/temp'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -9,5 +21,37 @@ def home():
     return "hello world!"
 
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print(file.filename)
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
 if __name__ == '__main__':
+    app.secret_key = 'password' #hahaha
     app.run(debug=True, host='0.0.0.0')
